69878acf04791a1a8841160ef3f607ac8b94db5b
anthony-b1 <anthony78510@gmail.com>
2022-06-15 16:46:38 -0400

    Create javascript_methods.js

---
+++ javascript_methods.js
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------
+JavaScript Prototype: Method
+Every object in JavaScript has a built-in property called "prototype." 
+The prototype constructor is used to add new methods (functions) and properties to a JavaScript object. 
+- If a method is constructed, then it will be available for the object. 
+- If a property is constructed, then the object will be given the property and its value, as default.
+In this Assignment, we use the prototype constructor to add new methods to the Array() object.
+----------------------------------------------------------*/
+
+// MAP //
+Array.prototype.myMap = function(callbackFn) {
+    // Place your code here.
+};
+
+// FILTER //
+Array.prototype.myFilter = function(callbackFn) {
+    // Place your code here.
+};
+
+// SOME //
+Array.prototype.mySome = function(callbackFn) {
+    // Place your code here.
+};
+
+// EVERY //
+Array.prototype.myEvery = function(callbackFn) {
+    // Place your code here.
+};
+
+// REDUCE //
+Array.prototype.myReduce = function(callbackFn) {
+    // Place your code here.
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function(searchElement) {
+    // Place your code here.
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function(searchElement) {
+    // Place your code here.
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function(searchElement) {
+    // Place your code here.
+};
+
+// KEYS //
+Object.myKeys = function(object) {
+    // Place your code here.
+};
+
+// VALUES //
+Object.myValues = function(object) {
+    // Place your code here.
+};
